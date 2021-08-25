46bebe9ac63d0e157542d3f3efbd0f53f0dc9b64
swbsf <s.wallimann@quortex.io>
2021-08-25 16:37:32 +0200

    test

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_s3_bucket" "quortex" {
   provisioner "local-exec" {
     when    = destroy
     command = <<EOT
-      ${self.force_destroy} && aws s3 rm s3://${self.bucket} --recursive --quiet
+aws s3 rm s3://${self.bucket} --recursive --quiet
     EOT
   }
 }
